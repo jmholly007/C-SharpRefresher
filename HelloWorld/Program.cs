09678bb5cffa5366a3e22f9bbcfd2b23975067c2
Joshua Holly <jm.holly007@gmail.com>
2022-05-15 16:39:44 -0500

    Added OOP

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,11 +1,56 @@
 ﻿using System; //means that we use classes from the System Namespace
+using System.Linq; //Other useful array methods, such as Min, Max, and Sum, can be found in the System.Linq namespace:
 
-namespace HelloWorld //used to orgnaize your code, and it is a container for lcasses and other namespaces
+namespace HelloWorld //used to orgnaize your code, and it is a container for classes and other namespaces
 {
+   
+    
+    class Car
+    {
+        string color = "red";
+        static void Object(string[] args)
+        {
+            Car myObj1 = new Car();
+            Car myOjb2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myOjb2.color);
+            
+        }
+
+    }
+    
+    
     class Program
     {
+
+
+        static int MyMethod(int x, int y)
+        {
+            //code to be executed when MyMethod() is called
+            return x + y;
+            
+        }
+
+        static void TheMethod(string fname = "Luka", string lname = "Doncic", int age = 22)
+        {
+            Console.WriteLine(fname + " " + lname + " is age "  + age );       
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting the program by calling MyMethod()");
+            int z = MyMethod(3, 4);
+            Console.WriteLine(z);
+            Console.WriteLine("Now callng TheMethod(s)");
+            TheMethod("Giannis", "Antek", 3);
+            TheMethod();
+            TheMethod("Josh", "Holly", 26);
+
+           /*
+            * 
+            * 
+            * 
+            *     
             
             Console.WriteLine("Hello World");
             Console.WriteLine("I am learning C#");
@@ -69,6 +114,8 @@ namespace HelloWorld //used to orgnaize your code, and it is a container for lca
            */
 
             //strings
+            
+        /*
             string txt = "adskfjalsdkjfa;lkdsjf";
             Console.WriteLine(txt.ToUpper());
             Console.WriteLine(txt.ToLower());
@@ -91,7 +138,66 @@ namespace HelloWorld //used to orgnaize your code, and it is a container for lca
 
 
 
+            /*
+
+            Console.WriteLine("Please enter a number");
+            int x = Convert.ToInt32(Console.ReadLine()); // converts the 
+            Console.WriteLine("Please enter a 2nd number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x < y)
+            {
+                Console.WriteLine(x + " is less than " + y);
+            }
+            else
+            {
+                Console.WriteLine(x + " is greater than " + y);
+
+            }
+       
+            int a = 3;
+            int b = 24;
+            while(a <= b)
+            {
+                Console.WriteLine("a is less than b. a = " +  a);
+                a++;
+            }
+             */
+        /*
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+            int[] cars = { 14, 26, day };
+            Console.WriteLine(cars[1]); // will print the contents of y as it is in the array's 1 position. 
+            Console.WriteLine(cars.Length); // will print the lenght of the array
+
+            for(int i=0; i<cars.Length;i++)
+             {
+                Console.WriteLine(cars[i]);
+            }
+        */
 
         }
     }
-}
